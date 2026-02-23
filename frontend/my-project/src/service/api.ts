a513d35d9ecff0ca460a88bf7fe2c8cef1dc7784
iamanzalshaikh <tocxanzalftw49@gmail.com>
2026-02-24 01:30:36 +0530

    Update api.ts

--- frontend/my-project/src/service/api.ts
+++ frontend/my-project/src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
-export const API_URL = "https://e8-laundary-backend.onrender.com";
+export const API_URL = "https://e8-laundary-backend.onrender.com/api/v1";
 
 export const api = axios.create({
   baseURL: API_URL,
